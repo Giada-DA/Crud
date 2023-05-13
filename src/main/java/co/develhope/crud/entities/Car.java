e80ae98a8bbd0e69b6c33a9cdce4083fefc4e6d8
Giada Dall'Ara <giada.25700@gmail.com>
2023-05-13 20:35:38 +0200

    Esercizio sui metodi CRUD

--- src/main/java/co/develhope/crud/entities/Car.java
+++ src/main/java/co/develhope/crud/entities/Car.java
@@ -1,9 +1,6 @@
 package co.develhope.crud.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
